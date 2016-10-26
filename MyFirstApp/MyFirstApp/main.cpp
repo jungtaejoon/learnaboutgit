3ff00ffbe10adbb61929bab1978e36bb14e8140c
jungtaejoon <jungtaejoon@naver.com>
2016-10-26 18:26:32 +0900

    함수 프로타입 .h 와 .cpp

---
+++ MyFirstApp/MyFirstApp/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+#include "prototypes.h"
+
+int main()
+{
+	cout << boolalpha << isPositive(4) << endl;
+	cout << absEqual(4, -4) << endl;
+}
---
+++ MyFirstApp/MyFirstApp/prototypes.h
@@ -0,0 +1,6 @@
+#pragma once
+extern int superglobal;
+
+bool isPositive(double value);
+bool absEqual(int a, int b);
+char getGrade(int grade);
