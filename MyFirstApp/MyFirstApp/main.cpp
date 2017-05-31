bbf56f4e4487f95303741ee44a2dbf879a79f408
jungtaejoon <jungtaejoon@naver.com>
2017-05-31 23:29:14 +0900

    index.html  생성

--- MyFirstApp/MyFirstApp/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-#include "prototypes.h"
-
-int main()
-{
-	cout << boolalpha << isPositive(4) << endl;
-	cout << absEqual(4, -4) << endl;
-}
--- MyFirstApp/MyFirstApp/prototypes.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-extern int superglobal;
-
-bool isPositive(double value);
-bool absEqual(int a, int b);
-char getGrade(int grade);
